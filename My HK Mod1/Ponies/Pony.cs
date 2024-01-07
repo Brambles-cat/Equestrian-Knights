3ca3afdb3d2c0c561d8b2e3bf07bb5b733f0458f
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2024-01-07 15:30:52 -0800

    Updated pony base class

--- My HK Mod1/Ponies/Pony.cs
+++ My HK Mod1/Ponies/Pony.cs
@@ -1,23 +1,67 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
 namespace PonyMod.Ponies
 {
+    // this is probably not optimized at all but whatever, i'll fix this later
     public abstract class Pony
     {
-        public Texture2D texture = new Texture2D(4096, 4096);
+        static bool logFailed = false, logSucceeded = true;
 
-        public Pony(string spriteSheetName) {
-            texture.LoadImage(File.ReadAllBytes($"{PonyMod.DataDirectory}/{spriteSheetName}.png"));
+        public Satchel.Animation[] anims = new Satchel.Animation[Enum.GetValues(typeof(AnimState)).Length];
+        public int hitpoints = 4;
+        public bool isCrystalized = false, canFly = false;
+        public Vector3 centerOffset;
+        public AnimState currentAnim = AnimState.idle;
+
+        public Pony(string ponyName, Vector3 centerOffset) {
+            foreach (AnimState state in Enum.GetValues(typeof(AnimState))) {
+                try {
+                    anims[(int) state] = Satchel.CustomAnimation.LoadAnimation(Path.Combine(PonyMod.DataDirectory, $"{ponyName}\\{state}.json"));
+                    if (logSucceeded) Modding.Logger.Log($"Loaded {ponyName}/{state}");
+                }
+                catch {
+                    if (logFailed) Modding.Logger.Log($"Animation {ponyName}/{state} failed to load");
+                }
+            }
+            this.centerOffset = centerOffset;
         }
 
         public static Pony
             currentPony = null!,
-            twilight = new TwilightSparkle("Twilight Sparkle"),
-            applejack = new Applejack("Applejack"),
-            fluttershy = new Fluttershy("Fluttershy"),
-            pinkie = new PinkiePie("Pinkie Pie"),
-            rainbow = new RainbowDash("Rainbow Dash"),
-            rarity = new Rarity("Rarity");
+            twilight = new TwilightSparkle("Twilight Sparkle", Vector3.zero),
+            applejack = new Applejack("Applejack", new Vector3(-0.2f, 0.1f, 0)),
+            fluttershy = new Fluttershy("Fluttershy", new Vector3(-0.6f, 0.1f, 0f)),
+            pinkie = new PinkiePie("Pinkie Pie", Vector3.zero),
+            rainbow = new RainbowDash("Rainbow Dash", Vector3.zero),
+            rarity = new Rarity("Rarity", Vector3.zero);
+
+        public static Pony getFromStr(string pony)
+        {
+            switch (pony)
+            {
+                case "twilight sparkle": return twilight;
+                case "applejack": return applejack;
+                case "fluttershy": return fluttershy;
+                case "pinkie pie": return pinkie;
+                case "rainbow dash": return rainbow;
+                case "rarity": return rarity;
+
+                default: throw new NotImplementedException($"invalid pony name: {pony}");
+            }
+        }
+
+        public enum AnimState
+        {
+            sit,
+            idle,
+            walk,
+            run,
+            air_ascending,
+            fly,
+            fall,
+
+        }
     }
 }
