11d35f147b47ad5e5e48471a55c68c5b1fe7724d
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2024-01-07 15:35:52 -0800

    Lots of other stuff i don't feel like writing about

--- My HK Mod1/SpriteLoader.cs
+++ My HK Mod1/SpriteLoader.cs
@@ -1,90 +1,77 @@
 ﻿using Logger = Modding.Logger;
 using UnityEngine;
-using System.IO;
 using PonyMod.Ponies;
+using System;
+using System.Collections.ObjectModel;
+using Satchel;
+using System.IO;
+using GlobalEnums;
 
-namespace PonyMod
-{
+namespace PonyMod {
     class SpriteLoader {
-
-        static tk2dSpriteDefinition[] defs = null!;
-        public static HeroAnimationController animCtrl = null!;
-
+        private static bool initialized = false;
+        public static GameObject displayedPony = new GameObject();
+        private static CustomAnimationController animCtrlr = null!;
         public static void init(On.HeroController.orig_Awake orig, HeroController self)
         {
-            animCtrl = self.GetComponent<HeroAnimationController>();
-            defs = self.GetComponent<tk2dSprite>().Collection.spriteDefinitions;
+            //Pony.currentPony = Pony.fluttershy;
             orig(self);
-            Logger.Log("awaken");
+            displayedPony.transform.SetParent(HeroController.instance.transform);
+            displayedPony.transform.position = HeroController.instance.transform.position;
+            animCtrlr = displayedPony.GetAddComponent<CustomAnimationController>();
+            
         }
 
-        public static void LoadSprites()
+        public static void StartSpritesTemp() // ponyswitcher
         {
+            displayedPony.transform.position = HeroController.instance.transform.position + Pony.currentPony.centerOffset;
+            initialized = true;
+        }
 
-            tk2dSpriteAnimationClip
-                clip = animCtrl.animator.GetClipByName("Run"),
-                nextClip = animCtrl.animator.GetClipByName("Slash");
-            HeroController.instance.gameObject.GetComponent<tk2dSprite>().GetCurrentSpriteDef().material.mainTexture = Pony.currentPony.texture;
-            Logger.Log(clip.frames.Length);
-            int i = 0;
-            foreach(var frame in clip.frames)
+        public static void AnimationUpdate()
+        {
+            if (!initialized) return;
+            ActorStates actorState = HeroController.instance.GetComponent<HeroAnimationController>().actorState;
+            
+            if (actorState == ActorStates.running)
             {
-                Logger.Log(frame.spriteId);
-                defs[frame.spriteId].indices = [0, 1, 2, 2, 3, 0];
-                defs[frame.spriteId]
-                    .uvs = [
-                        new Vector2((float)(220 * i) / 4096, 1),
-                        new Vector2((float)(220 * i) / 4096, 3902f / 4096f),
-                        new Vector2((float)(220 * (i + 1)) / 4096, 3902f / 4096f),
-                        new Vector2((float)(220 * (i + 1)) / 4096, 1)
-                    ];
-                defs[frame.spriteId]
-                    .positions = [
-                        new Vector3(-1.3f, 1.2f),
-                        new Vector3(-1.3f, -1.4f),
-                        new Vector3(1.3f, -1.4f),
-                        new Vector3(1.3f, 1.2f)
-                    ];
-                ++i;
+                playIfNotAlready(Pony.AnimState.walk);
+                animCtrlr.anim.fps = 32;
             }
-            /*for (int i = 0; i < 16; ++i)
+            else if (actorState == ActorStates.idle)
             {
-                defs[i + 9].indices = [0, 1, 2, 2, 3, 0];
-                defs[i + 9]
-                    .uvs = [
-                        new Vector2((float)(220 * i) / 4096, 1),
-                        new Vector2((float)(220 * i) / 4096, 3902f / 4096f),
-                        new Vector2((float)(220 * (i + 1)) / 4096, 3902f / 4096f),
-                        new Vector2((float)(220 * (i + 1)) / 4096, 1)
-                    ];
-                defs[i + 9]
-                    .positions = [
-                        new Vector3(-1.3f, 1.1f),
-                        new Vector3(-1.3f, -1.3f),
-                        new Vector3(1.3f, -1.3f),
-                        new Vector3(1.3f, 1.2f)
-                    ];
-            }*/
+                playIfNotAlready(Pony.AnimState.idle);
+            }
 
-            clip.fps = 20;
-            Logger.Log(clip.wrapMode);
-            clip.wrapMode = tk2dSpriteAnimationClip.WrapMode.Loop;
-            /*
-            tk2dSpriteAnimationFrame[] frames = new tk2dSpriteAnimationFrame[16];
-            int i2 = 0;
-            for (;i2 < clip.frames.Length; ++i2)
+            HeroControllerStates controllerStates = HeroController.instance.GetComponent<HeroControllerStates>();
+
+            if (controllerStates == null) return;
+
+            if(controllerStates.inWalkZone)
             {
-                frames[i2] = clip.frames[i2];
+                Logger.Log("in walk zone");
+                playIfNotAlready(Pony.AnimState.walk);
+                animCtrlr.anim.fps = 20;
             }
-            for(; i2 < 16; ++i2)
+        }
+
+        private static void playIfNotAlready(Pony.AnimState animation)
+        {
+            if (Pony.currentPony.currentAnim != animation)
             {
-                tk2dSpriteAnimationFrame frame = new tk2dSpriteAnimationFrame();
-                frame.spriteId = nextClip.frames[i2 - clip.frames.Length].spriteId;
-                frames[i2] = frame;
-            }*/
-            Logger.Log("Attempting build...");
-            Logger.Log(clip.frames.Length);
-            HeroController.instance.GetComponent<tk2dSprite>().ForceBuild();
-        } // 1 5 9 7 3 13 15
+                Pony.currentPony.currentAnim = animation;
+                animCtrlr.Init(Pony.currentPony.anims[(int) animation]);
+                animCtrlr.currentFrame = 0;
+                Modding.Logger.Log($"Playing: {animation}");
+            }
+        }
+
+        public static void play(Pony.AnimState animation)
+        {
+            Pony.currentPony.currentAnim = animation;
+            animCtrlr.Init(Pony.currentPony.anims[(int) animation]);
+            animCtrlr.currentFrame = 0;
+            Modding.Logger.Log($"Playing: {animation}");
+        }
     }
 }
